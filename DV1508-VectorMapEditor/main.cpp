9de5f20b1c4af4c64a81106908c41f3ab69d27d6
Trisslotten <tristan23@live.se>
2019-05-04 21:35:13 +0200

    add libs

--- DV1508-VectorMapEditor/main.cpp
+++ DV1508-VectorMapEditor/main.cpp
@@ -1,8 +1,56 @@
-#include <iostream>
+#include "window.hpp"
+#include "timer.hpp"
+#include "input.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
 
-
-int main()
+int main(void)
 {
-	std::cout << "Hello world!" << std::endl;
+	Window::open();
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+
+	ImGui_ImplOpenGL3_Init();
+
+	Timer frameTime;
+
+	char buf[256];
+	float f = 0;
+
+	while (!Window::shouldClose())
+	{
+		double dt = frameTime.elapsed();
+
+		io.DeltaTime = dt;
+		io.DisplaySize.x = Window::size().x;
+		io.DisplaySize.y = Window::size().y;
+		io.MousePos.x = Input::mousePosition().x;
+		io.MousePos.y = Input::mousePosition().y;
+		io.MouseDown[0] = Input::isMouseButtonDown(GLFW_MOUSE_BUTTON_1);
+		io.MouseDown[1] = Input::isMouseButtonDown(GLFW_MOUSE_BUTTON_2);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui::NewFrame();
+
+
+
+		ImGui::Text("Hello, world %d", 123);
+		if (ImGui::Button("Save"))
+		{
+			// do stuff
+		}
+		ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+
+
+		ImGui::EndFrame();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		Window::update();
+	}
+
+	Window::close();
 	return 0;
 }
---
+++ DV1508-VectorMapEditor/shader.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <unordered_map>
+#include <GL/glew.h>
+#include <string>
+#include <glm/glm.hpp>
+
+#define uniformDec(glmtype) \
+void uniformv(const std::string& name, GLuint count, const glmtype* values); \
+void uniform(const std::string& name, const glmtype & value);
+
+class ShaderProgram
+{
+	const std::string SHADERS_PATH = "shaders/";
+
+	std::unordered_map<GLenum, std::string> paths;
+	std::unordered_map<GLenum, GLuint> ids;
+
+	GLuint id;
+
+	GLuint findUniformLocation(const std::string& name);
+
+	std::unordered_map<std::string, GLuint> uniform_locations;
+
+	bool compiled = false;
+public:
+	ShaderProgram();
+	~ShaderProgram();
+
+	void add(GLenum type, const std::string& path);
+	void add(const std::string & path);
+	void compile();
+
+	void reload();
+
+	void use();
+
+	bool isCompiled() { return compiled; }
+
+	uniformDec(GLfloat);
+	uniformDec(glm::vec2);
+	uniformDec(glm::vec3);
+	uniformDec(glm::vec4);
+
+	uniformDec(GLint);
+	uniformDec(glm::ivec2);
+	uniformDec(glm::ivec3);
+	uniformDec(glm::ivec4);
+
+	uniformDec(GLuint);
+	uniformDec(glm::uvec2);
+	uniformDec(glm::uvec3);
+	uniformDec(glm::uvec4);
+
+	uniformDec(glm::mat2);
+	uniformDec(glm::mat3);
+	uniformDec(glm::mat4);
+
+	uniformDec(glm::mat2x3);
+	uniformDec(glm::mat3x2);
+
+	uniformDec(glm::mat2x4);
+	uniformDec(glm::mat4x2);
+
+	uniformDec(glm::mat3x4);
+	uniformDec(glm::mat4x3);
+
+	GLuint getId()
+	{
+		return this->id;
+	}
+};
